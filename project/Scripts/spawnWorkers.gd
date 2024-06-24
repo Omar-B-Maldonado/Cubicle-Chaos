eef1991ddcbc7df71ab842f962ed7c852d0649c7
Daniel Astacio <danielastacio91@gmail.com>
2024-06-23 23:17:01 -0400

    Adjusting spawn areas

--- project/Scripts/spawnWorkers.gd
+++ project/Scripts/spawnWorkers.gd
@@ -1,17 +1,22 @@
 extends Timer
 
+var spawnAreas = []
+
 func _ready():
 	self.start()
+	spawnAreas.append($SpawnArea1.position)
+	spawnAreas.append($SpawnArea2.position)
+	spawnAreas.append($SpawnArea3.position)	
+	self.wait_time = 10
 
 func _SpawnWorker():
 	
-	var randomNum = randf_range(0,300)
-	var randomPosition = Vector2(randomNum, randomNum)
+	var randomNum = randf_range(-1,spawnAreas.size())
 	var workerNode = preload("res://Scenes/worker.tscn")
 	var instance = workerNode.instantiate()
 	
 	add_child(instance)
-	instance.position = randomPosition
+	instance.position = spawnAreas[randomNum]
 	
 func _on_timeout():
 	_SpawnWorker()
